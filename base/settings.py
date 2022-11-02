2b9753e00b07c7ab39154384196481132a5554be
cao-trong-le <caomaxpro@DESKTOP-RR1BLJR.localdomain>
2022-11-02 15:09:23 -0400

    update_2_11_22_2

--- base/settings.py
+++ base/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from django.conf import settings
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,15 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get("DEBUG")) == "1" 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
-if not DEBUG:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    ALLOWED_HOSTS += [os.environ.get("ALLOWED_HOST")]
+# if not DEBUG:
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOST")]
 
 
 # Application definition
